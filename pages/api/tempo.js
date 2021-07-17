ad17c9d795baf474cc38df335ea46b8c200da211
LucasDarela <lucasdarela@live.com>
2021-07-17 12:43:42 -0300

    rota /api/tempo

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,9 +1,9 @@
-function tempo (request,response);{
+function tempo (request,response) {
     const dynamicDate = new Date();
 
     response.json({
         date: dyynamicDate.toGMTString()
-    })
+    });
 }
 
 export default tempo;
