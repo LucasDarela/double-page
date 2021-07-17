301c244520363293a4f320af8c5dd5d8cebbe321
LucasDarela <lucasdarela@live.com>
2021-07-17 15:10:38 -0300

    adiciona cache

--- pages/api/tempo.js
+++ pages/api/tempo.js
@@ -1,6 +1,8 @@
 function tempo(request, response) {
     const dynamicDate = new Date();
 
+    response.setHeader('Cache-Control', 's-maxage=10, stale-white-revalidade');
+
     response.json({
         date: dynamicDate.toGMTString()
     });
